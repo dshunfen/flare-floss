4bb366f9895bd191192cb15fd2bfcfbda2755730
David Shunfenthal <dshunfenthal@cyberpointllc.com>
2018-12-03 10:56:31 -0800

    Trying python-modernize on the repo to use with python 3

--- floss/__init__.py
+++ floss/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2017 FireEye, Inc. All Rights Reserved.
 
-from utils import ONE_MB
-from utils import STACK_MEM_NAME
-from utils import makeEmulator
-from utils import removeStackMemory
+from __future__ import absolute_import
+from .utils import ONE_MB
+from .utils import STACK_MEM_NAME
+from .utils import makeEmulator
+from .utils import removeStackMemory
